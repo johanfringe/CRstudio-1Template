efd5203078e40b56d13bbd991b10f39c3d285c72
Johan Lorrez <johan@lorrez.be>
2025-01-06 14:21:35 +0100

    End of day commit

--- .cache/page-ssr/routes/component---src-pages-index-js.js
+++ .cache/page-ssr/routes/component---src-pages-index-js.js
@@ -1,23 +1,18 @@
 "use strict";
-exports.id = "component---src-pages-index-js";
-exports.ids = ["component---src-pages-index-js"];
+exports.id = 293;
+exports.ids = [293];
 exports.modules = {
 
-/***/ "./src/pages/index.js?export=default":
-/*!*******************************************!*\
-  !*** ./src/pages/index.js?export=default ***!
-  \*******************************************/
+/***/ 9639:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6419);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-
-const HomePage = () => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h1", null, "Welkom op de homepage"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", null, "Dit is een testpagina."));
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HomePage);
+const HomePage=()=>/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div",null,/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h1",null,"Welkom op de homepage"),/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p",null,"Dit is een testpagina."));/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HomePage);
 
 /***/ })
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/johan/CRstudio/CRstudio-1Template/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/johan/CRstudio/CRstudio-1Template/src/pages/404.js")),
   "component---src-pages-admin-js": preferDefault(require("/Users/johan/CRstudio/CRstudio-1Template/src/pages/admin.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/johan/CRstudio/CRstudio-1Template/src/pages/index.js")),
