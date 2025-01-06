9fceb976e69532cb6a7befc39dde4e44ba9715e5
Johan Lorrez <johan@lorrez.be>
2025-01-06 15:09:49 +0100

    End of day commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ const NotFoundPage = () => {
   return (
     <main className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold">404: Page Not Found</h1>
-      <p className="text-lg mt-4 text-gray-600">The page you are looking for does not exist.</p>
+      <p className="text-lg mt-4 text-gray-600">The page you are looking for does not exist here.</p>
     </main>
   );
 };
